facaaedaf3fc47f22b6352a1c895c878e25c23e4
Matthew Chum <mattcwc@live.ca>
2016-07-25 01:04:52 -0400

    modifying demo

--- demo.py
+++ demo.py
@@ -1,14 +1,12 @@
-from util.firebase_util import put_delivery
+from util.mailbox_handler import MailboxHandler
 
 
 def main():
-    email = 'mailbox@semaphore.ca'
-    secret = '56yZNdR1DApjhiKNKS3jcElWzWYSCWEfWPxjYHYf'
-    database_url = 'https://smartbox-041.firebaseio.com'
-    mailbox_id = 'temp_fd5c7ba5-c2db-4923-8075-046cbead8173'
-    letters, magazines, newspapers, parcels = 0, 0, 0, 0
-    put_delivery(database_url, email, secret, mailbox_id, letters, magazines, newspapers, parcels)
-
+    _email = 'mailbox@semaphore.ca'
+    _mailbox_id = 'temp_fd5c7ba5-c2db-4923-8075-046cbead8173'
+    _mailbox = MailboxHandler(_email, _mailbox_id)
+    _letters, _magazines, _newspapers, _parcels = 0, 0, 0, 0
+    _mailbox.send_data(_letters, _magazines, _newspapers, _parcels)
 
 if __name__ == '__main__':
     main()
