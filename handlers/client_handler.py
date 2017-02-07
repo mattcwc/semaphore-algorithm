567bf12db73b1371e1269705d91d84f54feb98b0
mattcwc <mattjwc@gmail.com>
2017-02-07 11:54:36 -0500

    some logic change involving params that shouldnt affect result

--- handlers/client_handler.py
+++ handlers/client_handler.py
@@ -5,13 +5,11 @@ def process_data(db_url, email, secret, mailbox_id,
                  letters=0, magazines=0, newspapers=0, parcels=0):
     params = locals()
     prev_snap = get_snapshot(db_url, email, secret, mailbox_id)
-    put_data(db_url, email, secret, mailbox_id, 'snapshot', None,
-             letters, magazines, newspapers, parcels)
+    put_data(put_type='snapshots', **params)
 
     if len(prev_snap) > 0:
         prev_snap = prev_snap.values()[0]
         for k, v in prev_snap.iteritems():
             if k in params:  # Effectively removes the 'timestamp' key
                 params[k] = max(params[k] - v, 0)
-    params['put_type'] = 'deliveries'
-    put_data(**params)
+    put_data(put_type='deliveries', **params)
